c9c7a11748a5fd9c01f2d619ee83563678bed93f
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2022-02-07 13:27:40 +0300

    [chore] Refactoring

--- tree-automata-sync/Program.fs
+++ tree-automata-sync/Program.fs
@@ -1,231 +1,6 @@
 module tree_automata_sync.Program
 open System
 
-let inline __notImplemented__() = failwith "Not implemented!"
-let inline __unreachable__() = failwith "Unreachable!"
-let inline toString x = x.ToString()
-let inline join (s : string) (ss : string list) = String.Join(s, ss)
-
-module Map =
-    let findOrDefault map x = Map.tryFind x map |> Option.defaultValue x
-
-    let findOrApply f map x = Map.tryFind x map |> Option.defaultWith (fun () -> f x)
-
-module List =
-    let kfoldk f =
-        let rec kfoldk z xs k =
-            match xs with
-            | [] -> k z
-            | x::xs -> f z x (fun y -> kfoldk y xs k)
-        kfoldk
-
-    let inline cons x xs = x :: xs
-
-    let mapChoose f xs =
-        let rec mapChoose xs k =
-            match xs with
-            | [] -> k []
-            | x::xs ->
-                match f x with
-                | Some y -> mapChoose xs (fun ys -> k (y::ys))
-                | None -> None
-        mapChoose xs Some
-
-    let instantiate map = List.map (Map.findOrDefault map)
-
-    let product xss =
-        let rec product xss k =
-            match xss with
-            | [] -> k [[]]
-            | xs::xss ->
-                product xss (fun yss -> xs |> List.collect (fun x -> List.map (fun ys -> x :: ys) yss) |> k)
-        product xss id
-
-type opname = string
-type ident = string
-type term =
-    | Var of ident
-    | Apply of opname * term list
-    override x.ToString() =
-        match x with
-        | Var i -> i
-        | Apply(op, []) -> op
-        | Apply(op, ts) -> $"""%s{op}(%s{ts |> List.map toString |> join ", "})"""
-module Term =
-    let fold z fIdent fOpname =
-        let rec fold z = function
-            | Var i -> fIdent i z
-            | Apply(op, ts) -> List.fold fold (fOpname op z) ts
-        fold z
-
-    let map fIdent fOpname =
-        let rec map = function
-            | Var i -> fIdent i
-            | Apply(op, ts) -> Apply(fOpname op, List.map map ts)
-        map
-
-    let toLinearString = fold [] List.cons List.cons >> join ""
-
-    let mapVars f = map f id
-
-    let freeVars = fold Set.empty Set.add (fun _ -> id)
-
-    let Bottom = Apply("Bot", [])
-
-    let isBottom = (=) Bottom
-
-    let cut = function
-        | Var _ -> None
-        | Apply(opname, args) -> Some(opname, args)
-
-    let private trySubstituteIdent map ident = Map.findOrApply Var map ident
-
-    let instantiate instantiator = mapVars (trySubstituteIdent instantiator)
-
-    let substitute constrMap varMap = map (trySubstituteIdent varMap) (Map.findOrDefault constrMap)
-
-module TermList =
-    let isBottoms = List.forall Term.isBottom
-
-    let freeVars = List.map Term.freeVars >> Set.unionMany
-
-    let instantiate instantiator = List.map (Term.instantiate instantiator)
-
-    let cutHeads = List.mapChoose Term.cut >> Option.map List.unzip
-
-    let toString = List.map toString >> join ", "
-
-type pattern = Pattern of term list
-module Pattern =
-    let freeVars (Pattern pat) = TermList.freeVars pat
-
-    let matchPattern (Pattern termsPat) (Pattern termsInst) =
-        let rec matchWith ((constrMap, varMap) as maps) (termPat, termInst) k =
-            let termPat = Term.substitute constrMap varMap termPat
-            match termPat with
-            | Apply(fPat, argsPat) ->
-                match termInst with
-                | Apply(fInst, argsInst) ->
-                    let maps = if fPat = fInst then maps else Map.add fPat fInst constrMap, varMap
-                    matchListsWith maps (List.zip argsPat argsInst) k
-                | Var _ -> None
-            | Var idPat ->
-                match termInst with
-                | Var idInst when idPat = idInst -> k maps
-                | Apply _
-                | Var _ -> k (constrMap, Map.add idPat termInst varMap)
-        and matchListsWith maps pairs k = List.kfoldk matchWith maps pairs k
-        matchListsWith (Map.empty, Map.empty) (List.zip termsPat termsInst) Some
-
-    let instantiate instantiator (Pattern(pat)) = Pattern(TermList.instantiate instantiator pat)
-
-    let cutHeads (Pattern pat) =
-        if TermList.isBottoms pat then None else TermList.cutHeads pat
-
-    let toString (Pattern(pat)) = TermList.toString pat
-
-type state =
-    | SVar of ident
-    | CombinedState of opname list * state list // ``Delay'' states
-    | AutomatonApply of ident * pattern
-    | DeltaApply of ident * opname list * state list
-    override x.ToString() =
-        match x with
-        | SVar i -> i
-        | AutomatonApply(name, pat) -> $"%s{name}[%s{Pattern.toString pat}]"
-        | CombinedState(constrs, states) ->
-            let cs = constrs |> List.map toString |> join ", "
-            let ss = states |> List.map toString |> join ", "
-            $"""((%s{cs}), (%s{ss}))"""
-        | DeltaApply(name, constrs, states) ->
-            let cs = constrs |> List.map toString |> join ", "
-            let ss = states |> List.map toString |> join ", "
-            $"""delta%s{name}(%s{cs}, %s{ss})"""
-
-module State =
-    let mapAutomatonApplies f =
-        let rec mapPattern = function
-            | SVar _ as v -> v
-            | AutomatonApply(name, pattern) -> f name pattern
-            | DeltaApply(name, constrs, states) -> DeltaApply(name, constrs, List.map mapPattern states)
-            | CombinedState(constrs, states) -> CombinedState(constrs, List.map mapPattern states)
-        mapPattern
-
-    let foldAutomatonApplies f =
-        let rec fold z = function
-            | SVar _ -> z
-            | AutomatonApply(name, pattern) -> f z name pattern
-            | CombinedState(_, states)
-            | DeltaApply(_, _, states) -> List.fold fold z states
-        fold
-
-    let mapFoldAutomatonApplies f =
-        let rec mapFold z = function
-            | SVar _ as v -> v, z
-            | AutomatonApply(name, pattern) -> f z name pattern
-            | DeltaApply(name, heads, states) ->
-                let states, z = List.mapFold mapFold z states
-                DeltaApply(name, heads, states), z
-            | CombinedState(heads, states) ->
-                let states, z = List.mapFold mapFold z states
-                CombinedState(heads, states), z
-        mapFold
-
-    let private isConstructorFree (op : string) = op.Chars(0) |> Char.IsLower //TODO
-
-    let freeConstructors =
-        let rec freeConstructors free state =
-            match state with
-            | DeltaApply(_, constrs, states) ->
-                List.fold freeConstructors (constrs |> List.filter isConstructorFree |> Set.ofList |> Set.union free) states
-            | _ -> free
-        freeConstructors Set.empty >> Set.toList
-
-    let instantiate instantiator = mapAutomatonApplies (fun name pat -> AutomatonApply(name, Pattern.instantiate instantiator pat))
-
-    let rec unfoldAutomatonApply =
-        let bottomize tss =
-            let N = List.map List.length tss |> List.max
-            let padWithBottoms ts =
-                let P = N - List.length ts
-                if P = 0 then ts else
-                ts @ List.init P (fun _ -> Term.Bottom)
-            List.map padWithBottoms tss
-
-        let unfoldAutomatonApply name pattern =
-            match Pattern.cutHeads pattern with
-            | Some(heads, bodies) ->
-                let bodies = bottomize bodies
-                let states = List.product bodies |> List.map (fun pat -> AutomatonApply(name, Pattern pat))
-                let states = List.map unfoldAutomatonApply states
-                DeltaApply(name, heads, states)
-            | None -> AutomatonApply(name, pattern)
-        mapAutomatonApplies unfoldAutomatonApply
-
-    let freeVars = foldAutomatonApplies (fun free _ -> Pattern.freeVars >> Set.union free) Set.empty
-
-    let abstractAutomatonApplies =
-        let nameFromPattern (Pattern pat) =
-            pat
-            |> List.map Term.toLinearString
-            |> join ""
-            |> IdentGenerator.gensymp
-        let helper ((vars2states, states2vars) as maps) name pat =
-            let a = AutomatonApply(name, pat)
-            match Map.tryFind a states2vars with
-            | Some ident -> SVar ident, maps
-            | None ->
-                let freshName = nameFromPattern pat
-                SVar freshName, (Map.add freshName a vars2states, Map.add a freshName states2vars)
-        mapFoldAutomatonApplies helper (Map.empty, Map.empty)
-
-    let collectAutomatonApplies = foldAutomatonApplies (fun states name pat -> Set.add (AutomatonApply(name, pat)) states) Set.empty
-
-module Vars =
-    let toString = List.map toString >> join ", "
-
-    let instantiate instantiator = List.map (Map.findOrApply SVar instantiator)
-
 module Example =
     let private aname = IdentGenerator.gensymp "a"
     let private var n = Var (IdentGenerator.gensymp n)
@@ -237,8 +12,9 @@ module Example =
     let private af = binaryConstructor "af"
     let private cf = binaryConstructor "cf"
     let private df = binaryConstructor "df"
-
-    let freshVar () = Var (IdentGenerator.gensym ())
+    let unaryConstructor c = let c = IdentGenerator.gensymp c in fun x -> Apply(c, [x])
+    let private S = unaryConstructor "S"
+    let private af1 = unaryConstructor "af1"
 
     let pattern2a pattern = AutomatonApply("A", pattern)
     let pattern2b pattern = AutomatonApply("B", pattern |> Pattern.freeVars |> Set.toList |> List.map Var |> Pattern)
@@ -255,59 +31,25 @@ module Example =
         let pattern = Pattern [a; N(L, N(x, y))]
         let instantiator = Map.ofList [ aname, af(c1, df(d1, d2)) ]
 
+    module Example4 =
+        let pattern = Pattern [a; S(x)]
+        let instantiator = Map.ofList [ aname, af1(y) ]
+
 let printQuery A B =
     printfn "Хотим:"
-    printfn $"""%O{B}\in Fb <=> %O{A} \in Fa"""
+    printfn $"""{B} \in Fb <=> {A} \in Fa"""
     printfn ""
 
 let printInstance instantiator A B =
     let A' = State.instantiate instantiator A
     let B' = State.instantiate instantiator B
     printfn "Инстанцируем:"
-    printfn $"""%O{B'} \in Fb <=>"""
-    printfn $"""  Fa \ni %O{A'} ="""
+    printfn $"""{B'} \in Fb <=>"""
+    printfn $"""  Fa \ni {A'} ="""
     let A'' = State.unfoldAutomatonApply A'
-    printfn $"\t %O{A''}"
+    printfn $"\t {A''}"
     printfn ""
     A'', B'
-
-type invariant =
-    | Invariant of opname list * state list
-    override x.ToString() =
-        match x with
-        | Invariant(freeConstrs, abstrValues) ->
-            let freeConstrsStr = freeConstrs |> List.map toString |> join ", "
-            $"""(({freeConstrsStr}), ({abstrValues |> List.map toString |> join ", "}))"""
-
-module Invariant =
-    let private mapEachState f (Invariant(freeConstrs, states)) = Invariant(freeConstrs, List.map f states)
-
-    let instantiate instantiator = mapEachState (State.instantiate instantiator)
-
-    let mapAutomatonApplies f = mapEachState (State.mapAutomatonApplies f)
-
-    let unfoldAutomatonApply = mapEachState State.unfoldAutomatonApply
-
-    let private matchAutomatonApplyStates statePattern stateInstance =
-        match statePattern, stateInstance with
-        | AutomatonApply(namePat, termsPat), AutomatonApply(nameInst, termsInst) ->
-            if namePat = nameInst then Pattern.matchPattern termsPat termsInst else None
-        | _ -> __unreachable__()
-
-    let rewrite state (rewriteFromState, Invariant(rewriteToConstrs, rewriteToStates)) =
-        let rec rewrite = function
-            | AutomatonApply _ as a ->
-                match matchAutomatonApplyStates rewriteFromState a with
-                | Some(substConstrs, substStates) ->
-                    let constrs' = List.instantiate substConstrs rewriteToConstrs
-                    let states' = List.map (State.instantiate substStates) rewriteToStates
-                    CombinedState(constrs', states')
-                | None -> a
-            | DeltaApply(name, constrs, states) -> DeltaApply(name, constrs, List.map rewrite states)
-            | _ -> __notImplemented__()
-        rewrite state
-
-    let collectAutomatonApplies (Invariant(_, states)) = states |> List.map State.collectAutomatonApplies |> Set.unionMany
 
 let printFinal A B =
     printfn "Положим:"
@@ -315,19 +57,20 @@ let printFinal A B =
     let abstrVars = abstrVarsMap |> Map.toList |> List.map fst
     let freeConstrs = State.freeConstructors abstrState
     let freeConstrsStr = freeConstrs |> List.map toString |> join ", "
-    printfn $"Fb := {{ (({freeConstrsStr}), ({abstrVars |> Vars.toString})) |\n\t%O{abstrState} \in Fa }}"
+    printfn $"""Fb := {{ (({freeConstrsStr}), ({abstrVars |> List.map toString |> join ", "})) |{"\n\t"}{abstrState} \in Fa }}"""
     printfn ""
     printfn "Тогда инвариант:"
-    let inv = Invariant(freeConstrs, Vars.instantiate abstrVarsMap abstrVars)
-    printfn $"""%O{B} = {inv}"""
+    let inv = Invariant(freeConstrs, List.map (Map.findOrApply SVar abstrVarsMap) abstrVars)
+    printfn $"""{B} = {inv}"""
     printfn ""
     inv
 
 let printInduction B invariantA =
     let freeVars = State.freeVars B |> Set.toList
+    let N = max (State.maxFunctionArity B) (Invariant.maxFunctionArity invariantA)
     let instantiator=
         freeVars
-        |> List.map (fun ident -> ident, Example.binaryConstructor ident (Example.freshVar(), Example.freshVar())) //TODO: not only binary
+        |> List.map (fun ident -> ident, Term.mkApplyNary N ident (fun _ -> Term.mkFreshVar ()))
         |> Map.ofList
     printfn "Индукционная раскрутка слева:"
     let B' = State.instantiate instantiator B
@@ -371,8 +114,10 @@ let printInductionSchema leftSide rightSide =
 
 [<EntryPoint>]
 let main _ =
-    let pattern = Example.Example3.pattern
-    let instantiator = Example.Example3.instantiator
+    Console.OutputEncoding <- System.Text.Encoding.UTF8
+
+    let pattern = Example.Example4.pattern
+    let instantiator = Example.Example4.instantiator
 
     let A = Example.pattern2a pattern
     let B = Example.pattern2b pattern
